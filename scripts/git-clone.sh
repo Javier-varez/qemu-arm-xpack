4e37eab7ef8a6bede2787f16db3c1d0f79ea71ac
Francisco Javier Alvarez Garcia <javier.alvarez@allthingsembedded.net>
2020-02-05 23:05:31 +0100

    update clone script

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf "${HOME}/Downloads/qemu-arm-xpack.git"
-git clone --recurse-submodules https://github.com/xpack-dev-tools/qemu-arm-xpack.git "${HOME}/Downloads/qemu-arm-xpack.git"
+git clone --recurse-submodules --branch use_bluepill_branch https://github.com/javier-varez/qemu-arm-xpack.git "${HOME}/Downloads/qemu-arm-xpack.git"
